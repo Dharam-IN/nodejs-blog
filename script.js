8d88170234b93a9d5169ca8301a9a1678ad37703
dharamraj-dev <dharamrajyadav6312@gmail.com>
2024-01-24 12:28:59 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express();
+
+
+// Middleware
+// app.use(function(req, res, next){
+//     // console.log("middleware working")
+//     next();
+//     console.log(res)
+// })
+
+
+// EJS File
+app.set("view engine", "ejs")
+
+// Routers
+app.get("/", function(req,res){
+    res.render("index")
+})
+
+app.get("/profile", function(req, res){
+    res.send("Hello From Profile")
+})
+
+app.get("/profile/:dynamicurl", function(req, res){
+    res.send(`Hello from ${req.params.dynamicurl}`)
+})
+
+app.listen(3001)
